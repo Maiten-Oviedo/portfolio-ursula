9c77a2ea9e52a5580917a8585509daa7b01419cc
Maitén Oviedo <maitenoviedo513@gmail.com>
2024-08-09 22:38:48 -0300

    Arrglo de bug

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -20,6 +20,14 @@ const NavBar = () => {
     }
   }, [])
 
+  // Función para manejar el clic en un enlace y cerrar el menú
+  const handleMenuClick = () => {
+    if (window.innerWidth < 640) {
+      // Verifica si el tamaño de pantalla es menor a 'sm'
+      setMenuOpen(false)
+    }
+  }
+
   const bgStyle = {
     backgroundColor: scrollPosition > 2 || menuOpen ? '#f4f4f5' : 'transparent',
     boxShadow:
@@ -29,12 +37,12 @@ const NavBar = () => {
   return (
     <>
       <nav
-        className="fixed z-20 px-[5%] lg:px-[15%] w-full flex justify-between items-center p-4 transition font-bold"
+        className="fixed z-20 px-[5%] lg:px-[15%] w-full flex justify-between items-center p-3 sm:p-4 transition font-bold"
         style={bgStyle}
       >
         <h1 className="text-5xl italic font-normal">Úrsula Castro</h1>
         <button
-          className="block lg:hidden p-2 text-zinc-900 rounded focus:outline-none"
+          className="block lg:hidden text-zinc-900 rounded focus:outline-none"
           onClick={() => setMenuOpen(!menuOpen)}
         >
           <svg
@@ -44,41 +52,40 @@ const NavBar = () => {
             stroke="#0d0d0d"
             className="size-12"
           >
-            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-            <g
-              id="SVGRepo_tracerCarrier"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            ></g>
-            <g id="SVGRepo_iconCarrier">
-              {' '}
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M4 5C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H4ZM7 12C7 11.4477 7.44772 11 8 11H20C20.5523 11 21 11.4477 21 12C21 12.5523 20.5523 13 20 13H8C7.44772 13 7 12.5523 7 12ZM13 18C13 17.4477 13.4477 17 14 17H20C20.5523 17 21 17.4477 21 18C21 18.5523 20.5523 19 20 19H14C13.4477 19 13 18.5523 13 18Z"
-                fill="#000000"
-              ></path>{' '}
-            </g>
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M4 5C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H4ZM7 12C7 11.4477 7.44772 11 8 11H20C20.5523 11 21 11.4477 21 12C21 12.5523 20.5523 13 20 13H8C7.44772 13 7 12.5523 7 12ZM13 18C13 17.4477 13.4477 17 14 17H20C20.5523 17 21 17.4477 21 18C21 18.5523 20.5523 19 20 19H14C13.4477 19 13 18.5523 13 18Z"
+              fill="#000000"
+            ></path>
           </svg>
         </button>
         <ul
           className={`list-none gap-8 items-center text-zinc-950 transition-transform duration-300 ease-in-out ${
             menuOpen
-              ? ' absolute top-20 left-0 w-full bg-[#f4f4f5] flex flex-col justify-center items-center shadow-md p-4'
+              ? 'absolute top-20 left-0 w-full bg-[#f4f4f5] flex flex-col justify-center items-center shadow-md p-4'
               : 'hidden'
           } lg:flex`}
         >
           <li className="hover:text-[#31a0eb] hover:scale-[1.02] transition-500">
-            <a href="#">INICIO</a>
+            <a href="#" onClick={handleMenuClick}>
+              INICIO
+            </a>
           </li>
           <li className="hover:text-[#31a0eb] hover:scale-[1.02] transition-500">
-            <a href="#experience">EXPERIENCIA</a>
+            <a href="#experience" onClick={handleMenuClick}>
+              EXPERIENCIA
+            </a>
           </li>
           <li className="hover:text-[#31a0eb] hover:scale-[1.02] transition-500">
-            <a href="#about">SOBRE MÍ</a>
+            <a href="#about" onClick={handleMenuClick}>
+              SOBRE MÍ
+            </a>
           </li>
           <li className="hover:text-[#31a0eb] hover:scale-[1.02] transition-500">
-            <a href="#contact">CONTACTO</a>
+            <a href="#contact" onClick={handleMenuClick}>
+              CONTACTO
+            </a>
           </li>
         </ul>
       </nav>
